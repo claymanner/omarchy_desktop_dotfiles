2364511ae852257b46e687a41de128ca07f58665
claymanner <clayton@mannerow.net>
2025-11-17 01:04:52 -0500

    added blink config

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,9 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<CR>"] = {}, -- Unbind Enter completely
+      ["<C-y>"] = { "accept" }, -- Use Ctrl+y to accept completions
+    },
+  },
+}
